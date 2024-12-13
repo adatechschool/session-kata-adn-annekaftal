082967966a46372126e00038b353d203a126cea5
annekaftal <anne.kaftal@gmail.com>
2024-12-13 10:22:10 +0100

    Add: array of codons

--- adn/src/main.js
+++ adn/src/main.js
@@ -1,3 +1,14 @@
+import { log } from "console";
 import { readFileSync } from "fs";
-const text = readFileSync("adn/adn.txt").toString().split("\n");
-console.log(text);
+let text = readFileSync("adn/adn.txt").toString().split("\n")[0];
+// console.log(text);
+
+// Étape 1 : découper le fichier en codons tant qu'il reste au moins 3 nucléotides
+const codons = [];
+while (text.length >= 3) {
+  let i = 0;
+  let codon = text[i] + text[i + 1] + text[i + 2];
+  codons.push(codon);
+  text = text.slice(i + 3, text.length);
+}
+console.log(codons);
